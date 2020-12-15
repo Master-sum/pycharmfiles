e121578ef2c83a2f76a14dc34ae6b2b327ca3259
Master-sum <285957109@qq.com>
2020-12-15 23:47:01 +0800

    xlwings使用转换xlsx

--- numpy/numpy01.py
+++ numpy/numpy01.py
@@ -6,4 +6,15 @@
 坚持：山穷水复疑无路，柳暗花明又一村
 """
 import numpy as np
-#
+import xlwings as xw
+# 初始化
+wb = xw.Book()
+sht = wb.sheets[0]
+info_title = [
+    ['21','hello','shanghai'],
+    ['22','hello','shanghai']
+]
+title = [['title','world','crity']]
+sht.range('a1').value = title
+sht.range('a2').value = info_title
+wb.save('/Users/baijinxing/Documents/files/t.xlsx')
